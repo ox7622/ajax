d206d33e658e2a75e80def68639e4abb55702dd5
Oxana Shovkoplyas <oshovkoply001@cee.ema.ad.pwcinternal.com>
2016-11-22 15:20:55 +0300

    added  all 3 APIs

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -14,8 +14,60 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+         var streetName = $("input:text#street").val();
+    
+    var cityName = $('input#city').val();
+    var address = streetName + ", " + cityName;
+    var imgURL= "http://maps.googleapis.com/maps/api/streetview?size=600x300&location="+ address + " ";
+    $body.append('<img class="bgimg" src="' + imgURL + '">');
+
+//NYTimes AJAX request
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "266e8c7e6639429c92b5200758ac03fd",
+  'q': cityName,
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+$nytHeaderElem.text("New York Times articles about " + cityName);
+var articles= result.response.docs;
+for (var i=0; i<articles.length; i++) {
+    var article=articles[i];
+    $nytElem.append('<li class="article"><a href="'+ article.web_url +'">'+ article.headline.main +'</a>'+'<p>'+ article.snippet+'</p>'+'</li>');
+};
+
+}).fail(function(err) {
+  $nytHeaderElem.text("New York Times articles about " + cityName+" cannot be loaded");
+});
+
+//Wiki
+
+var wikiURL = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+ cityName + "&format=json&callback=wikiCallback";
+var wikiRequestTimeout =  setTimeout(function() {
+    $("#wikipedia-header").text("Wikipedia links about " + cityName + " cannot be loaded. sorry!");
+}, 9000 );
+
+$.ajax({
+    url: wikiURL,
+    dataType:  "jsonp",
+}).done(function(data) {
+    $("#wikipedia-header").text("Wikipedia links about " + cityName);
+    var articles= data[1];
+
+    for (var j=0; j<articles.length; j++) {
+    var article=articles[j];
+    var url="http://en.wikipedia.org/wiki" + article;
+    $wikiElem.append('<li><a href="'+ url +'">'+ article +'</a>'+ '</li>');
+    };
+    clearTimeout(wikiRequestTimeout);
+    });
+//<p><'+ article'</p>
 
     return false;
 };
 
 $('#form-container').submit(loadData);
+
+
